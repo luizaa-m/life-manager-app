0e5e735cf3ce74350b8ae489ed54f0cc32f77088
luizaa-m <luiza.martins0310@proton.me>
2024-10-16 21:06:03 -0300

    feature: updating dependencies and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import NavBar from "./components/nav-bar";
+import { NavBarNestedListItemProps } from "./components/nav-bar-nested-list-item/interface/nav-bar-nested-list-item-interface";
+import { NavBarProps } from "./components/nav-bar/interface/nav-bar-interface";
+import { HiCurrencyDollar, HiCog } from "react-icons/hi";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const getNavBarProps = () => {
+    return {
+      title:'Life Manager',
+      items: [
+        {
+          primaryText: "Financeiro",
+          primaryIcon: <HiCurrencyDollar color='white' />,
+          items: [
+            { itemText: "Categoria Gasto", itemIcon: <HiCog  color='white' />, link: "" },
+          ],
+        } as NavBarNestedListItemProps,
+      ],
+    } as NavBarProps;
+  };
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <NavBar {...getNavBarProps()} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
